809681b4ad7482fd2dff7128b15d43662f7ef34e
juliakarst <84075127+juliakarst@users.noreply.github.com>
2022-04-07 12:52:04 +0200

    Create merge_csv.py

---
+++ csv_data_and_scripts/merge_csv.py
@@ -0,0 +1,26 @@
+import csv
+import glob
+
+"This python-file will merge multiple smaller csv-files to one big one"
+header = []
+data = []
+
+file_list = glob.glob('/Users/juliakarst/PycharmProjects/NewsWebcrawler/sueddeutsche/*.csv')
+
+
+def merge_data(input_files):
+    for file in input_files:
+        with open(file, 'r') as readingfile:
+            reader = csv.reader(readingfile, delimiter=',')
+            header = next(reader)
+            data.extend([row for row in reader])
+
+
+
+
+merge_data(file_list)
+
+with open('sueddeutsche_all.csv', 'w') as result:
+    writer = csv.writer(result, delimiter=',')
+    writer.writerow(header)
+    writer.writerows(data)
